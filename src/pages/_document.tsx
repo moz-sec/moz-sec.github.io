a6cfc50f02b0fd27506f3f887206e71759ddb56d
moz-sec <m0253c@gmail.com>
2025-07-19 15:19:29 +0900

    feat: change the favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,94 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        {/* Basic */}
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="format-detection" content="telephone=no" />
+
+        {/* Favicons  */}
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/favicon/android-chrome-512x512.png"
+        />
+
+        {/* Windows Tile */}
+        <meta
+          name="msapplication-TileImage"
+          content="/favicon/mstile-150x150.png"
+        />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#bbc0e4" />
+
+        {/* Font */}
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato:100,700,400&display=optional"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K2MF784"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Main />
         <NextScript />
+
+        {/* Scripts */}
+        <Script
+          src="https://www.googletagmanager.com/gtm.js?id=GTM-K2MF784"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="https://kit.fontawesome.com/0f38bfd7d6.js"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0ZQV7CECY7"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-inline" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0ZQV7CECY7');
+          `}
+        </Script>
       </body>
     </Html>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Script from "next/script";
+
 import About from "./components/About";
 import Skills from "./components/Skill";
 import Navbar from "./components/NavBar";
@@ -12,58 +12,37 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>moz_sec_</title>
+        <title>moz-sec</title>
         <meta
           name="description"
-          content="mozのホームページです。ネットワークやセキュリティを勉強しています。"
+          content="mozのホームページへようこそ。ネットワークとセキュリティの沼に浸かっています。"
         />
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no,"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <meta name="format-detection" content="telephone=no" />
+
+        {/* Open Graph  */}
         <meta property="og:url" content="https://moz-sec.com/index.html" />
         <meta
           property="og:image"
           content="https://moz-sec.com/image/hello_world.png"
         />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="moz-sec HOME" />
+        <meta property="og:title" content="moz-sec" />
         <meta
           property="og:description"
-          content="mozのホームページです。ネットワークやセキュリティを勉強しています。"
+          content="mozのホームページへようこそ。ネットワークとセキュリティの沼に浸かっています。"
         />
         <meta property="og:locale" content="ja_JP" />
+
+        {/* Twitter Card  */}
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@moz_sec_" />
+
+        {/* Facebook  */}
         <meta property="fb:app_id" content="907921083813066" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#2b5797" />
-        <meta name="theme-color" content="#bbc0e4" />
       </Head>
-      <Script
-        src="https://www.googletagmanager.com/gtm.js?id=GTM-K2MF784"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://kit.fontawesome.com/0f38bfd7d6.js"
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-      />
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-0ZQV7CECY7"
-        strategy="afterInteractive"
-      />
-      <Script id="ga-inline" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-0ZQV7CECY7');
-      `}
-      </Script>
 
       <main>
         <Navbar />
